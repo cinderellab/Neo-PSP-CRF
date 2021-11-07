a0f3574735ce3579cd62532925b305fa7f302278
cinderellab <cinderela80@proton.me>
2021-11-07 14:57:32 +0000

    export/hazard:

--- include/cnpy.h
+++ include/cnpy.h
@@ -128,4 +128,15 @@ namespace cnpy {
 
         if(fp) {
             //zip file exists. we need to add a new npy file to it.
-            //first read the footer. this gives u
+            //first read the footer. this gives us the offset and size of the global header
+            //then read and store the global header. 
+            //below, we will write the the new data at the start of the global header then append the global header and footer below it
+            unsigned int global_header_size;
+            parse_zip_footer(fp,nrecs,global_header_size,global_header_offset);
+            fseek(fp,global_header_offset,SEEK_SET);
+            global_header.resize(global_header_size);
+            size_t res = fread(&global_header[0],sizeof(char),global_header_size,fp);
+            if(res != global_header_size){
+                throw std::runtime_error("npz_save: header read error while adding to existing zip");
+            }
+            fseek(fp,glob
