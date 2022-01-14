de765372031da0d4a6a3fd303af174799b0105be
cinderellab <cinderela80@proton.me>
2022-01-14 20:29:26 +0000

    update dir

--- include/densecrf.h
+++ include/densecrf.h
@@ -154,4 +154,20 @@ public:
 	// Add  a pairwise potential defined over some feature space
 	// The potential will have the form:    w*exp(-0.5*|f_i - f_j|^2)
 	// The kernel shape should be captured by transforming the
-	// features before passi
+	// features before passing them into this function
+	void addPairwiseEnergy( const float * features1, const float * features2, int D, float w=1.0f, const SemiMetricFunction * function=NULL );
+	
+	// Add your own favorite pairwise potential (ownwership will be transfered to this class)
+	void addPairwiseEnergy( PairwisePotential* potential12, PairwisePotential* potential21 );
+	
+	// Run inference and return the probabilities
+	void inference( int n_iterations, float* result1, float * result2, float relax=1 );
+	
+	// Run MAP inference and return the map for each pixel
+	void map( int n_iterations, short int* result1, short int* result2, float relax=1 );
+	
+	// Access the two CRF's directly
+	DenseCRF& getCRF( int i );
+	const DenseCRF& getCRF( int i ) const;
+	
+	// Step by ste
