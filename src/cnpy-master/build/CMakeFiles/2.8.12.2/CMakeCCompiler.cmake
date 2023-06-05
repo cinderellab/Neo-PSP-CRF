a6c6ae078082075a42ed671b4cbf2f9ed92da1f5
cinderellab <cinderela80@proton.me>
2023-06-05 20:54:30 +0000

    add block

--- src/cnpy-master/build/CMakeFiles/2.8.12.2/CMakeCCompiler.cmake
+++ src/cnpy-master/build/CMakeFiles/2.8.12.2/CMakeCCompiler.cmake
@@ -38,3 +38,19 @@ if(CMAKE_C_SIZEOF_DATA_PTR)
 endif()
 
 if(CMAKE_C_COMPILER_ABI)
+  set(CMAKE_INTERNAL_PLATFORM_ABI "${CMAKE_C_COMPILER_ABI}")
+endif()
+
+if(CMAKE_C_LIBRARY_ARCHITECTURE)
+  set(CMAKE_LIBRARY_ARCHITECTURE "x86_64-linux-gnu")
+endif()
+
+
+
+
+set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "c")
+set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "/usr/lib/gcc/x86_64-linux-gnu/4.8;/usr/lib/x86_64-linux-gnu;/usr/lib;/lib/x86_64-linux-gnu;/lib")
+set(CMAKE_C_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
+
+
+
