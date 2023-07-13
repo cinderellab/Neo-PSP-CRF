86d083ca536141b8058587f9a83fef1d72d51aa0
cinderellab <cinderela80@proton.me>
2023-07-13 16:37:19 +0000

    Add codecov badge

---
+++ src/cnpy-master/example1.cpp
@@ -0,0 +1,15 @@
+#include"cnpy.h"
+#include<complex>
+#include<cstdlib>
+#include<iostream>
+#include<map>
+#include<string>
+
+const int Nx = 128;
+const int Ny = 64;
+const int Nz = 32;
+
+int main()
+{
+    //create random data
+    std::complex<double>* data = ne
