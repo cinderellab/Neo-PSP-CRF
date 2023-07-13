c9bc51e14dbbb321339b6df5ddff52e356ccd9a8
cinderellab <cinderela80@proton.me>
2023-07-13 17:34:07 +0000

    Documentation typo

--- src/cnpy-master/example1.cpp
+++ src/cnpy-master/example1.cpp
@@ -12,4 +12,21 @@ const int Nz = 32;
 int main()
 {
     //create random data
-    std::complex<double>* data = ne
+    std::complex<double>* data = new std::complex<double>[Nx*Ny*Nz];
+    for(int i = 0;i < Nx*Ny*Nz;i++) data[i] = std::complex<double>(rand(),rand());
+
+    //save it to file
+    const unsigned int shape[] = {Nz,Ny,Nx};
+    cnpy::npy_save("arr1.npy",data,shape,3,"w");
+
+    //load it into a new array
+    cnpy::NpyArray arr = cnpy::npy_load("arr1.npy");
+    std::complex<double>* loaded_data = reinterpret_cast<std::complex<double>*>(arr.data);
+    
+    //make sure the loaded data matches the saved data
+    assert(arr.word_size == sizeof(std::complex<double>));
+    assert(arr.shape.size() == 3 && arr.shape[0] == Nz && arr.shape[1] == Ny && arr.shape[2] == Nx);
+    for(int i = 0; i < Nx*Ny*Nz;i++) assert(data[i] == loaded_data[i]);
+
+    //append the same data to file
+    //npy array 
