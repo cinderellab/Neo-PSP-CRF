bfef3a4ca77074e01a525099fd67b4cdab25ace5
cinderellab <cinderela80@proton.me>
2023-08-26 18:45:25 +0000

    Bump version to 2.2.0

--- src/densecrf_lib/build/cmake_install.cmake
+++ src/densecrf_lib/build/cmake_install.cmake
@@ -27,4 +27,23 @@ IF(NOT CMAKE_INSTALL_COMPONENT)
   ENDIF(COMPONENT)
 ENDIF(NOT CMAKE_INSTALL_COMPONENT)
 
-# I
+# Install shared libraries without execute permission?
+IF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  SET(CMAKE_INSTALL_SO_NO_EXE "1")
+ENDIF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+
+IF(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  INCLUDE("/home/xduser/lihuan/densecrf_lib/build/src/cmake_install.cmake")
+  INCLUDE("/home/xduser/lihuan/densecrf_lib/build/examples/cmake_install.cmake")
+
+ENDIF(NOT CMAKE_INSTALL_LOCAL_ONLY)
+
+IF(CMAKE_INSTALL_COMPONENT)
+  SET(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+ELSE(CMAKE_INSTALL_COMPONENT)
+  SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+ENDIF(CMAKE_INSTALL_COMPONENT)
+
+FILE(WRITE "/home/xduser/lihuan/densecrf_lib/build/${CMAKE_INSTALL_MANIFEST}" "")
+FOREACH(fil
