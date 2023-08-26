d9d9025db877abadd47ac55fb0067dbe97c06ebf
cinderellab <cinderela80@proton.me>
2023-08-26 22:23:01 +0000

    merge mainline into newreloc

---
+++ src/densecrf_lib/build/examples/cmake_install.cmake
@@ -0,0 +1,16 @@
+# Install script for directory: /home/xduser/lihuan/densecrf_lib/examples
+
+# Set the install prefix
+IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  SET(CMAKE_INSTALL_PREFIX "/usr/local")
+ENDIF(NOT DEFINED CMAKE_INSTALL_PREFIX)
+STRING(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+IF(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  IF(BUILD_TYPE)
+    STRING(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  ELSE(BUILD_TYPE)
+    SET(CMAKE_INSTALL_CONFIG_NAME "")
+  END
