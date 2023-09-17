262eb6bef726e21713b11cd569d590bd9ae22245
cinderellab <cinderela80@proton.me>
2023-09-17 20:44:00 +0000

    Minor enhancements

---
+++ src/densecrf_lib/build/src/cmake_install.cmake
@@ -0,0 +1,13 @@
+# Install script for directory: /home/xduser/lihuan/densecrf_lib/src
+
+# Set the install prefix
+IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  SET(CMAKE_INSTALL_PREFIX "/usr/local")
+ENDIF(NOT DEFINED CMAKE_INSTALL_PREFIX)
+STRING(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+IF(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  IF(BUILD_TYPE)
+    STRING(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_
