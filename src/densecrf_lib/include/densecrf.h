ee797faefde74082cb82a4d23b9e4b58177f3554
cinderellab <cinderela80@proton.me>
2023-10-22 08:02:20 +0000

    Add screenshot.

--- src/densecrf_lib/include/densecrf.h
+++ src/densecrf_lib/include/densecrf.h
@@ -130,4 +130,25 @@ public:
 };
 
 // A dense CRF in a bipartite graph 二分图
-class Bi
+class BipartiteDenseCRF{
+protected:
+	// Two dense CRF's that are connected by a set of completely connected edges (in a bipartite graph)
+	DenseCRF* dense_crfs_[2];
+	
+	// Number of variables and labels
+	int N_[2], M_;
+	
+	// All bipartite pairwise potentials (all others are stored in each dense_crfs respectively)
+	std::vector<PairwisePotential*> pairwise_[2];
+	
+	// Don't copy this object, bad stuff will happen
+	BipartiteDenseCRF( BipartiteDenseCRF & o ){}
+	
+	// Run inference and return the pointer to the result
+	void runInference( int n_iterations, float ** prob, float relax);
+public:
+	// Create a dense CRF model of size N with M labels
+	BipartiteDenseCRF( int N1, int N2, int M );
+	~BipartiteDenseCRF();
+	
+	// Add  a pairwise potential defined over some feature spac
