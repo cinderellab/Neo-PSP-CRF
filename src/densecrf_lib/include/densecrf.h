40b6a668e8365cdbcd883eecc2fb4a559999c324
cinderellab <cinderela80@proton.me>
2023-10-29 08:41:12 +0000

    int_sqrt: Improve 3x faster integer sqrt.

--- src/densecrf_lib/include/densecrf.h
+++ src/densecrf_lib/include/densecrf.h
@@ -191,4 +191,9 @@ public:
     // Use different source and target features
     Filter( const float * source_features, int N_source, const float * target_features, int N_target, int feature_dim );
     // Use the same source and target features
-    Filter( const float * features, 
+    Filter( const float * features, int N, int feature_dim );
+    //
+    ~Filter();
+    // Filter a bunch of values
+    void filter( const float * source, float * target, int value_size );
+};
